b9c631a00be122ab700609af2f2a79bf1eed776e
ogu83 <oguzkoroglu@gmail.com>
2021-10-27 23:27:58 +0300

    working

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -35,11 +35,11 @@ namespace MyCryptoMarket_MVC.Controllers
         public IActionResult OnGet24hTickers(DxRequestBase args)
         {
             var query = _context.Tickers.AsQueryable();
-            var totalCount = query.Count();
+            var totalCount =  _context.Tickers.Count();
 
-            if (args.Take > 0)
+            if (args.DxFilter != null && args.DxFilter.Any())
             {
-                query = query.Take(args.Take);
+                DynamicLinqExtentions.
             }
 
             if (args.Skip > 0)
@@ -47,9 +47,15 @@ namespace MyCryptoMarket_MVC.Controllers
                 query = query.Skip(args.Skip);
             }
 
+            if (args.Take > 0)
+            {
+                query = query.Take(args.Take);
+            }
+
             var tickers = query.ToList();
             var model = new DxGridResponse<Models.Ticker>(tickers, totalCount);
             var retval = new JsonResult(model);
+
             return retval;
         }
 
