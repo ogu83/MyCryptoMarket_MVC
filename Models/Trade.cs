35e94985bf674e0670e8c32f94068d9a7073b452
Oguz Koroglu <Oguz.Koroglu@haibooks.com>
2021-11-11 14:43:40 +0300

    BalanceLayout

--- Models/Trade.cs
+++ Models/Trade.cs
@@ -5,10 +5,14 @@ namespace MyCryptoMarket_MVC.Models
 {
     public enum TradeType { BUY, SELL }
 
+    public enum TradeStatus { ORDERED, PARTIAL_FILLED, FILLED, CANCELED }
+
     public class Trade
     {
         [Key]
         public int Id { get; set; }
+
+        public int Symbol_Id { get; set; }        
 
         public DateTime OrderDate { get; set; } = DateTime.Now;
 
@@ -18,8 +22,18 @@ namespace MyCryptoMarket_MVC.Models
 
         public decimal Amount { get; set; }
 
+        public decimal FilledAmount { get; set; }
+
+        public decimal RemainAmount { get { return Amount - FilledAmount; } }                    
+
         public decimal Price { get; set; }
 
         public decimal Total { get { return Amount * Price; } }
+
+        public decimal FilledTotal { get { return FilledAmount * Price; } }
+
+        public decimal RemainTotal { get { return RemainAmount * Price; } }
+
+        public TradeStatus Status { get; set; } = TradeStatus.ORDERED;
     }
 }
